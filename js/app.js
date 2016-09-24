d179ccbb8e59c09dc730f7810e980b0cd90215a3
Ashlinn Brennan <asbren13@gmail.com>
2016-09-24 14:35:24 -0400

    first commit with initial setup and first draft

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+  $('.next').click(function(){
+    $('.left').removeClass('disabled');
+    $('.question-box1').addClass('hide');
+    $('.question-box2').removeClass('hide');
+  });
+  $('.left').click(function(){
+    $('.question-box2').addClass('hide');
+    $('.question-box1').removeClass('hide');
+    $('.left').addClass('disabled');
+  });
+
+  $('.assessment-button').click(function(){
+    $('.form').removeClass('hide');
+    $('.assessment-button').addClass('hide');
+    $('.profile').addClass('hide');
+  });
+  $('.submit').click(function(){
+    $('.form').addClass('hide');
+    $('.assessment-button').removeClass('hide');
+    $('.profile').removeClass('hide');
+  });
+});
+
